5765b2bdf9d3fb07c595ac61e12c2908375d29b9
Finn Hoffmann <hoffmann_finn@gmx.de>
2024-12-18 15:21:34 +0100

    added GUM database from universial dependencys for in-domain analysis

--- src/main.py
+++ src/main.py
@@ -3,14 +3,17 @@ from preprocess import load_conllu_data
 from train import calculate_probabilities_from_data
 from evaluate import evaluate_pos_tagger_accuracy, evaluate_pos_tagger_common_errors, evaluate_pos_tagger_errors_by_sentence_length, evaluate_pos_tagger_errors_for_rare_words
 
-file_path_train = "../data/UD_English-EWT/en_ewt-ud-train.conllu"
-file_path_test = "../data/UD_English-EWT/en_ewt-ud-test.conllu"
+file_path_train_ewt = "../data/UD_English-EWT/en_ewt-ud-train.conllu"
+file_path_test_ewt = "../data/UD_English-EWT/en_ewt-ud-test.conllu"
 
-data = load_conllu_data(file_path_train)
+file_path_train_gum = "../data/UD_English-GUM/en_gum-ud-train.conllu"
+file_path_test_gum = "../data/UD_English-GUM/en_gum-ud-test.conllu"
+
+data = load_conllu_data(file_path_train_gum)
 initial_probs, transition_probs, emission_probs = calculate_probabilities_from_data(data)
 
-# evaluate_pos_tagger_accuracy(initial_probs, transition_probs, emission_probs, file_path_test)
-# evaluate_pos_tagger_common_errors(initial_probs, transition_probs, emission_probs, file_path_test)
-# evaluate_pos_tagger_errors_by_sentence_length(initial_probs, transition_probs, emission_probs, file_path_test)
-evaluate_pos_tagger_errors_for_rare_words(file_path_train, file_path_test)
+evaluate_pos_tagger_accuracy(initial_probs, transition_probs, emission_probs, file_path_test_gum)
+evaluate_pos_tagger_common_errors(initial_probs, transition_probs, emission_probs, file_path_test_gum)
+evaluate_pos_tagger_errors_by_sentence_length(initial_probs, transition_probs, emission_probs, file_path_test_gum)
+# evaluate_pos_tagger_errors_for_rare_words(file_path_train, file_path_test)
 
